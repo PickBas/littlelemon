8bf348d2c11434ab8cc2a19dd96bafa2632d48d6
Kirill Saied <sayed.kirill@gmail.com>
2023-04-27 17:10:06 +0300

    added models

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255, null=False, blank=False)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
+
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255, null=False, blank=False)
+    no_of_guests = models.SmallIntegerField()
+    booking_date = models.DateTimeField()
